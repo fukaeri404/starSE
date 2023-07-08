611e8c4eb60b6b551eba37c3bbfa64d6898e38c9
fukaeri404 <aungnyeinhan00@gmail.com>
2023-07-08 22:19:07 +0630

    prob7 used Set instead of ArrayList

--- src/main/java/com/starse/prob7/FindSameItemMain.java
+++ src/main/java/com/starse/prob7/FindSameItemMain.java
@@ -1,31 +1,36 @@
 package com.starse.prob7;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindSameItemMain {
 	public static void main(String[] args) {
 		String[] allThings = { "Pen", "Cat", "Dog", "Blue", "White", "Dog", "Pen", "Blue", "Yellow", "Pen", "Cat",
 				"Dog", "Blue", "White", "Dog", "Pen", "Blue", "Yellow" };
-		ArrayList<String> nonRepeat = new ArrayList<>();
-		for (String thing : allThings) {
-			if (nonRepeat.isEmpty())
-				nonRepeat.add(thing);
-			else {
-				for (int i = 0; i < nonRepeat.size(); i++) {
-					if(nonRepeat.get(i).equalsIgnoreCase(thing))break;
-					if(i == nonRepeat.size()-1) nonRepeat.add(thing);
-				}
-			}
-		}
-		
-		for(String nonR : nonRepeat) {
+//		ArrayList<String> nonRepeat = new ArrayList<>();
+//		for (String thing : allThings) {
+//			if (nonRepeat.isEmpty())
+//				nonRepeat.add(thing);
+//			else {
+//				for (int i = 0; i < nonRepeat.size(); i++) {
+//					if(nonRepeat.get(i).equalsIgnoreCase(thing))break;
+//					if(i == nonRepeat.size()-1) nonRepeat.add(thing);
+//				}
+//			}
+//		}
+
+		Set<String> uniqueThing = new HashSet<>();
+		for (String thing : allThings)
+			uniqueThing.add(thing);
+
+		for (String unique : uniqueThing) {
 			int count = 0;
 			for (String thing : allThings) {
-				if(nonR == thing) {
+				if (unique.equalsIgnoreCase(thing)) {
 					count++;
 				}
 			}
-			System.out.println(nonR + ": " + count +" times");
+			System.out.println(unique + ": " + count + " times");
 		}
 	}
 }
